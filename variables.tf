d608d59abd224f9e6a1e80eec3215397e5aa1107
Osman <mohamed.osman@cbussuper.com.au>
2020-05-07 11:01:56 +1000

    removed default bus

--- variables.tf
+++ variables.tf
@@ -2,5 +2,4 @@ variable "aws_region" {
   default     = "ap-southeast-2"
 }
 variable "auth0_event_bus" {
-  default     = "aws.partner/auth0.com/mosman-cfd0c6a6-f34d-409c-9613-4207ebac6671/auth0.logs"
 }
